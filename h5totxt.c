a8d781fa24c2758963e573728742457abb97803b
stevenj <none>
1999-12-06 17:40:56 -0500

    added h5totxt
    darcs-hash:19991206224056-f3bf6-95bfbb10ee7422f884249821991125d50304bf5a

---
+++ h5totxt.c
@@ -0,0 +1,212 @@
+/* Copyright (c) 1999 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#define COPYRIGHT \
+"Copyright (c) 1999 Massachusetts Institute of Technology\n"\
+"\n"\
+"Permission is hereby granted, free of charge, to any person obtaining\n"\
+"a copy of this software and associated documentation files (the\n"\
+"\"Software\"), to deal in the Software without restriction, including\n"\
+"without limitation the rights to use, copy, modify, merge, publish,\n"\
+"distribute, sublicense, and/or sell copies of the Software, and to\n"\
+"permit persons to whom the Software is furnished to do so, subject to\n"\
+"the following conditions:\n"\
+"\n"\
+"The above copyright notice and this permission notice shall be\n"\
+"included in all copies or substantial portions of the Software.\n"\
+"\n"\
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,\n"\
+"EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n"\
+"MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n"\
+"IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY\n"\
+"CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,\n"\
+"TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE\n"\
+"SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#include "config.h"
+#include "arrayh5.h"
+
+#define CHECK(cond, msg) { if (!(cond)) { fprintf(stderr, "h5totxt error: %s\n", msg); exit(EXIT_FAILURE); } }
+
+void usage(FILE *f)
+{
+     fprintf(f, "Usage: h5totxt [options] [<filenames>]\n"
+	     "Options:\n"
+	     "         -h : this help message\n"
+             "         -V : print version number and copyright\n"
+	     "         -v : verbose output\n"
+	     "   -s <sep> : use <sep> to separate columns [ default: "," ]\n"
+	     "  -o <file> : output to <file> (first input file only)\n"
+	     "    -x <ix> : take x=<ix> slice of data\n"
+	     "    -y <iy> : take y=<iy> slice of data\n"
+	     "    -z <iz> : take z=<iz> slice of data [ default: z=0 ]\n"
+	     "         -T : transpose the data [default: no]\n"
+	     "  -d <name> : use dataset <name> in the input file (default: first dataset)\n"
+	  );
+}
+
+int main(int argc, char **argv)
+{
+     arrayh5 a;
+     char *txt_fname = NULL, *data_name = NULL;
+     extern char *optarg;
+     extern int optind;
+     int c;
+     int slicedim = 2, islice = 0;
+     int err;
+     int nx, ny;
+     int verbose = 0;
+     int transpose = 0;
+     char *sep;
+     int ifile;
+
+     sep = (char*) malloc(sizeof(char) * 2);
+     CHECK(sep, "out of memory");
+     strcpy(sep, ",");
+
+     while ((c = getopt(argc, argv, "ho:x:y:z:d:vTs:V")) != -1)
+	  switch (c) {
+	      case 'h':
+		   usage(stdout);
+		   return EXIT_SUCCESS;
+	      case 'V':
+		   printf("h5totxt " VERSION " by Steven G. Johnson\n" 
+			  COPYRIGHT);
+		   return EXIT_SUCCESS;
+	      case 'v':
+		   verbose = 1;
+		   break;
+	      case 'T':
+		   transpose = 1;
+		   break;
+	      case 'o':
+		   txt_fname = (char*) malloc(sizeof(char) *
+					      (strlen(optarg) + 1));
+		   CHECK(txt_fname, "out of memory");
+		   strcpy(txt_fname, optarg);
+		   break;
+	      case 's':
+		   free(sep);
+		   sep = (char*) malloc(sizeof(char) *
+					(strlen(optarg) + 1));
+		   CHECK(sep, "out of memory");
+		   strcpy(sep, optarg);
+		   break;
+	      case 'd':
+		   data_name = (char*) malloc(sizeof(char) *
+					      (strlen(optarg) + 1));
+		   CHECK(data_name, "out of memory");
+		   strcpy(data_name, optarg);
+		   break;		   
+	      case 'x':
+		   islice = atoi(optarg);
+		   slicedim = 0;
+		   break;
+	      case 'y':
+		   islice = atoi(optarg);
+		   slicedim = 1;
+		   break;
+	      case 'z':
+		   islice = atoi(optarg);
+		   slicedim = 2;
+		   break;
+	      default:
+		   fprintf(stderr, "Invalid argument -%c\n", c);
+		   usage(stderr);
+		   return EXIT_FAILURE;
+	  }
+     if (optind == argc) {  /* no parameters left */
+	  usage(stderr);
+	  return EXIT_FAILURE;
+     }
+
+     for (ifile = optind; ifile < argc; ++ifile) {
+	  if (verbose)
+	       printf("reading from \"%s\", slice at %d in %c dimension.\n",
+		      argv[ifile], islice, slicedim + 'x');
+	  
+	  err = arrayh5_read(&a, argv[ifile], data_name, slicedim, islice);
+	  CHECK(!err, arrayh5_read_strerror[err]);
+	  CHECK(a.rank >= 1, "data must have at least one dimension");
+	  
+	  {
+	       double a_min, a_max;
+	       arrayh5_getrange(a, &a_min, &a_max);
+	       if (verbose)
+		    printf("data ranges from %g to %g.\n", a_min, a_max);
+	  }
+	  
+	  nx = a.dims[0];
+	  ny = a.rank < 2 ? 1 : a.dims[1];
+	  
+	  if (verbose)
+	       printf("writing %s from %dx%d input data.\n",
+		      txt_fname ? txt_fname : "to stdout", nx, ny);
+
+	  {
+	       FILE *f;
+	       int i, j;
+
+	       if (txt_fname) {
+		    f = fopen(txt_fname, "w");
+		    CHECK(f, "error creating file");
+	       }
+	       else
+		    f = stdout;
+	       
+	       if (transpose)
+		    for (j = 0; j < ny; ++j) {
+			 if (nx > 0)
+			      fprintf(f, "%g", a.data[0*ny + j]);
+			 for (i = 1; i < nx; ++i)
+			      fprintf(f, "%s%g", sep, a.data[i*ny + j]);
+			 fprintf(f, "\n");
+		    }
+	       else
+		    for (i = 0; i < nx; ++i) {
+			 if (ny > 0)
+			      fprintf(f, "%g", a.data[i*ny + 0]);
+			 for (j = 1; j < ny; ++j)
+			      fprintf(f, "%s%g", sep, a.data[i*ny + j]);
+			 fprintf(f, "\n");
+		    }
+
+	       if (txt_fname)
+		    fclose(f);
+	  }
+
+	  arrayh5_destroy(a);
+	  if (txt_fname)
+	       free(txt_fname);
+	  txt_fname = NULL;
+     }
+     free(sep);
+
+     return EXIT_SUCCESS;
+}
