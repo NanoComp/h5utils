830b7f9b5eafe9ad9ee99129066099fd20814255
Steven G. Johnson <stevenj@mit.edu>
2017-07-19 12:12:56 -0400

    pass through arbitrary configure arguments in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-configure_args=""
-
-while test $# -ge 1; do
-    case $1 in
-	--enable-*) configure_args="$configure_args $1" ;;
-	--disable-*) configure_args="$configure_args $1" ;;
-	--with-*) configure_args="$configure_args $1" ;;
-	--without-*) configure_args="$configure_args $1" ;;
-	*) echo "unknown argument $1"; exit 1 ;;
-    esac
-    shift
-done
-
 # paranoia: sometimes autoconf doesn't get things right the first time
 autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 
-./configure --with-hdf4 --enable-maintainer-mode $configure_args
+./configure --with-hdf4 --enable-maintainer-mode $*
